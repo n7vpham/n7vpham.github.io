f926228be63d591c416fce049380484638d8b815
nPham <namnhpham@gmail.com>
2024-04-21 15:56:36 -0400

    Fixing

--- itis3135/project/scripts/initial.js
+++ itis3135/project/scripts/initial.js
@@ -2,7 +2,7 @@ let lastScrollTop = 0;
 const header = document.querySelector('header');
 
 window.addEventListener('scroll', function() {
-    var scrollTop = window.scrolly || document.documentElement.scrollTop;
+    var scrollTop = window.scrollY || document.documentElement.scrollTop;
     
     if (scrollTop > lastScrollTop) {
         header.style.top = '-76px';
