749a71d483c3d9d189f0d04c23df61d5f6aabdaa
nPham <namnhpham@gmail.com>
2024-05-05 11:22:50 -0400

    Updating Project

--- itis3135/project/scripts/home.js
+++ itis3135/project/scripts/home.js
@@ -1,28 +1,14 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+var slideIndex = 0;
+carousel();
 
-function plusSlides(n) {
-    showSlides(slideIndex += n);
-}
-
-function currentSlide(n) {
-    showSlides(slideIndex = n);
-}
-
-function showSlides(n) {
-    let i;
-    let slides = document.getElementsByClassName("mySlides");
-    let dots = document.getElementsByClassName("thumb");
-    let captionText = document.getElementById("caption");
-    if (n > slides.length) { slideIndex = 1 }
-    if (n < 1) { slideIndex = slides.length }
-    for (i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
+function carousel() {
+    var i;
+    var x = document.getElementsByClassName("fade");
+    for (i = 0; i < x.length; i++) {
+        x[i].style.display = "none";
     }
-    for (i = 0; i < dots.length; i++) {
-        dots[i].className = dots[i].className.replace(" active", "");
-    }
-    slides[slideIndex - 1].style.display = "block";
-    dots[slideIndex - 1].className += " active";
-    captionText.innerHTML = `${dots[slideIndex - 1].alt} <sup style="font-size:13px";}>${slideIndex}/7</sup>`;
+    slideIndex++;
+    if (slideIndex > x.length) { slideIndex = 1 }
+    x[slideIndex - 1].style.display = "block";
+    setTimeout(carousel, 2000);
 }
